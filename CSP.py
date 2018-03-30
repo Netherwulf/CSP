6a6be07b7d79f310978a082445a4ac039a38b570
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-03-30 03:45:34 +0200

    Backtracking for map coloring almost done

--- CSP.py
+++ CSP.py
@@ -3,50 +3,66 @@ import numpy as np
 
 class CSP(object):
     def __init__(self, forward_checking=False, size=5, choose_random_color=False, choose_vertical_variable=False):
-        self.forward_checking=forward_checking
-        self.size=size
-        self.color_matrix=np.empty((size, size), int)
-        self.chosen_color_matrix = #macierz kolorów jakie już były wybrane
-        self.choose_random_color=choose_random_color
-        self.choose_vertical_variable=choose_vertical_variable
-        self.step_number=0
-        self.return_number=0
-        self.domain=np.arrange(size)
-        #POSZUKAC JAK UTWORZYC TABELE DWUWYMIAROWA Z TABELAMI W KAZDEJ KMORCE (TAKA SAMA TABELA W KAZDEJ KOMORCE)
+        self.forward_checking = forward_checking
+        self.size = size
+        self.color_matrix = np.empty((size, size), int)
+        self.domain = np.arange(1) + 1
+        self.chosen_colors_matrix = np.empty([self.size, self.size, 1], dtype=int)
+        self.choose_random_color = choose_random_color
+        self.choose_vertical_variable = choose_vertical_variable
+        self.step_number = 0
+        self.return_number = 0
+        # POSZUKAC JAK UTWORZYC TABELE DWUWYMIAROWA Z TABELAMI W KAZDEJ KMORCE (TAKA SAMA TABELA W KAZDEJ KOMORCE)
         pass
 
-    #DOPISAC WARTOSC BEZWZGLEDNA W SPRAWDZANIU ROZNICY
     def validate(self, row, column, value):
-        #sprawdzanie wartości na sąsiednich polach w pionie
-        if (self.size-1)>row>0:
-            if self.color_matrix[row-1][column] - value < 2 or self.color_matrix[row+1][column] - value < 2:
+        # sprawdzanie wartości na sąsiednich polach w pionie
+        if (self.size - 1) > row > 0:
+            if np.absolute(self.color_matrix[row - 1][column] - value) < 2 or np.absolute(
+                    self.color_matrix[row + 1][column] - value) < 2:
                 return False
-        if row==(self.size-1):
-            if self.color_matrix[row-1][column] - value < 2:
+        if row == (self.size - 1):
+            if np.absolute(self.color_matrix[row - 1][column] - value) < 2:
                 return False
-        if row==0:
-            if self.color_matrix[row+1][column] - value < 2:
+        if row == 0:
+            if np.absolute(self.color_matrix[row + 1][column] - value) < 2:
                 return False
-        #sprawdzenie wartości na sąsiednich polach w poziomie
-        if (self.size-1)>column>0:
-            if self.color_matrix[row][column-1] - value < 2 or self.color_matrix[row][column+1] - value < 2:
+        # sprawdzenie wartości na sąsiednich polach w poziomie
+        if (self.size - 1) > column > 0:
+            if np.absolute(self.color_matrix[row][column - 1] - value) < 2 or np.absolute(
+                    self.color_matrix[row][column + 1] - value) < 2:
                 return False
-        if column==self.size-1:
-            if self.color_matrix[row][column-1] - value < 2:
+        if column == self.size - 1:
+            if np.absolute(self.color_matrix[row][column - 1] - value) < 2:
                 return False
-        if column==0:
-            if self.color_matrix[row][column+1] - value < 2:
+        if column == 0:
+            if np.absolute(self.color_matrix[row][column + 1] - value) < 2:
                 return False
-        #sprawdzenie po skosie
-        if row>0 and column>0:
-            if self.color_matrix[row-1][column-1] - value < 1:
+        # sprawdzenie po skosie
+        if row > 0 and column > 0:
+            if np.absolute(self.color_matrix[row - 1][column - 1] - value) < 1:
                 return False
-        if row<self.size-1 and column<self.size-1:
-            if self.color_matrix[row+1][column+1] - value < 1:
+        if row < self.size - 1 and column < self.size - 1:
+            if np.absolute(self.color_matrix[row + 1][column + 1] - value) < 1:
                 return False
+        return True
 
     def solve_graph_colorization_backtracking(self):
-        for row in range(self.size):
-            for column in range(self.size):
-
-
+        row = 0
+        column = 0
+        while row < self.size:
+            while column < self.size:
+                for color in self.domain:
+                    self.chosen_colors_matrix[row][column] = np.append(self.chosen_colors_matrix[row][column], color)
+                    if self.validate(row, column, color):
+                        self.color_matrix[row][column] = color
+                        break
+                if self.color_matrix[row][column] not in self.domain:
+                    if column >= 1:
+                        column -= 2
+                    else:
+                        row -= 2
+                        column = self.size-1
+                        break
+                column += 1
+            row += 1
--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
 import CSP
+import numpy as np
 
 csp = CSP.CSP()
+
+domain = np.arange(2)
+# chosen_colors_matrix = np.where(x == 0, np.array([]), np.array([]))
+chosen_colors_matrix = np.ones([5, 5, 1], dtype=int)
+print(chosen_colors_matrix)
