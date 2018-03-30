a89d6a2010aea871be6f3b910945e65e29781f9b
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-03-30 04:32:28 +0200

    Backtracking fix #1

--- CSP.py
+++ CSP.py
@@ -5,9 +5,9 @@ class CSP(object):
     def __init__(self, forward_checking=False, size=5, choose_random_color=False, choose_vertical_variable=False):
         self.forward_checking = forward_checking
         self.size = size
-        self.color_matrix = np.empty((size, size), int)
-        self.domain = np.arange(1) + 1
-        self.chosen_colors_matrix = np.empty([self.size, self.size, 1], dtype=int)
+        self.color_matrix = np.zeros((self.size, self.size), int)
+        self.domain = np.arange(2) + 1
+        self.chosen_colors_matrix = np.zeros([self.size, self.size, self.domain.size], dtype=int)
         self.choose_random_color = choose_random_color
         self.choose_vertical_variable = choose_vertical_variable
         self.step_number = 0
@@ -52,12 +52,15 @@ class CSP(object):
         column = 0
         while row < self.size:
             while column < self.size:
-                for color in self.domain:
-                    self.chosen_colors_matrix[row][column] = np.append(self.chosen_colors_matrix[row][column], color)
+                # print("Domena: " + str(self.domain))
+                # print("Wybrane kolory" + str(self.chosen_colors_matrix[0][0]))
+                print("Wiersz: " + str(row) + " Kolumna: " + str(column))
+                for color in np.setdiff1d(self.domain, self.chosen_colors_matrix[row][column]):
+                    self.chosen_colors_matrix[row][column][color-1] = color
                     if self.validate(row, column, color):
                         self.color_matrix[row][column] = color
                         break
-                if self.color_matrix[row][column] not in self.domain:
+                if self.color_matrix[row][column] == 0:
                     if column >= 1:
                         column -= 2
                     else:
@@ -65,4 +68,10 @@ class CSP(object):
                         column = self.size-1
                         break
                 column += 1
-            row += 1
+            if row == 0 and np.array_equal(self.domain, self.chosen_colors_matrix[0][0]):
+                print("Nie udało się rozwiązać dla dziedziny: " + str(self.domain))
+                self.domain = np.append(self.domain, self.domain.size+1)
+                self.chosen_colors_matrix[0][0] = np.zeros(self.domain.size)
+            else:
+                row += 1
+        print(self.color_matrix)
--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 import CSP
 import numpy as np
 
-csp = CSP.CSP()
-
-domain = np.arange(2)
-# chosen_colors_matrix = np.where(x == 0, np.array([]), np.array([]))
-chosen_colors_matrix = np.ones([5, 5, 1], dtype=int)
-print(chosen_colors_matrix)
+csp = CSP.CSP(size=3)
+csp.solve_graph_colorization_backtracking()
+# domain = np.arange(2)
+# # chosen_colors_matrix = np.where(x == 0, np.array([]), np.array([]))
+# chosen_colors_matrix = np.zeros([5, 5, 5], dtype=int)
+# chosen_colors_matrix[1][1][5-1] = 5
+# print(np.array_equal(np.array([1, 2]), np.array([2, 1])))
